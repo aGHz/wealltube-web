edf0a09fe14c552f63e8910f2f53b9fd5df47e34
Adrian Ghizaru <adrian.ghizaru@gmail.com>
2024-05-09 15:36:29 -0400

    Fix VideoCard image scaling

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -6,7 +6,7 @@ interface VideoCardProps {
 
 function VideoCard({ video }: VideoCardProps) {
   return (
-    <div className="w-80 h-48 cursor-pointer select-none hover:shadow-xl" style={{ backgroundImage: `url('${video.image}')`}} >
+    <div className="w-80 h-48 cursor-pointer select-none hover:shadow-xl bg-cover" style={{ backgroundImage: `url('${video.image}')`}} >
       <div className="w-full h-full flex flex-col justify-end font-sofia-condensed text-xl hover:text-2xl text-atlantis-100 py-2 px-4 bg-gradient-to-b from-transparent via-transparent via-50% to-atlantis-950-a0.8">
         {video.title}
       </div>
